e5739285d64bfe7e7f5cb9bb24423a311b1c9a6b
Scott Laird <laird@google.com>
2019-05-07 18:56:29 -0700

    Remove some cobra boilerplate that slipped through, and rename the --hostname flag to --camera, as it's clearer.

--- virb/cmd/root.go
+++ virb/cmd/root.go
@@ -33,16 +33,10 @@ var hostname string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "virb",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Talk to a Garmin Virb camera over WiFi",
+	Long: `The virb tool talks to Garmin Virb cameras over the network and
+can be used for start and stop recording, fetch images, list and set
+configuration entries, and so on.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -61,7 +55,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.virb.yaml)")
-	rootCmd.PersistentFlags().StringVar(&hostname, "hostname", "10.1.0.180", "hostname")
+	rootCmd.PersistentFlags().StringVar(&hostname, "camera", "10.1.0.180", "hostname")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
